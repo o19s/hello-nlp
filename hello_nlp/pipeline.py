f3a61aabaae591ad011d6a556580794883c19616
Max Irwin <max_irwin@yahoo.com>
2020-09-24 13:35:14 -0400

    Pipeline Dreams

--- hello_nlp/pipeline.py
+++ hello_nlp/pipeline.py
@@ -8,7 +8,7 @@ pipelines = {
 	"payloader":payload.Payloader
 }
 
-class Pipeline:
+class Analyzer:
 	def analyze(self,text: str) -> str:
 		data = text
 		for stage in self.stages:
@@ -16,46 +16,50 @@ class Pipeline:
 		text = data
 		return text
 
-	def __init__(self,analyzers):
-		self.metadata = {"model":"en_core_web_lg"}
+	def __init__(self,analyzers,nlp):
 		self.stages = []
+		self.metadata = {"nlp":nlp}
 		for stage in analyzers:
 			if stage in pipelines.keys():
 				self.stages.append(pipelines[stage](self.metadata))
 		print(self.metadata)
 
+class Field:
+	def analyze(self,text: str) -> str:
+		return analyzer.analyze(text)
+
+	def __init__(self,field:dict,analyzer:Analyzer):
+		self.source = field["source"]
+		self.target = field["target"]
+		self.analyzer = analyzer
+
 class Pipelines:
-	def add(self,field):
-		self.fields[field["field"]] = Pipeline(field["pipeline"])
+
+	def enrich(self,document:dict) -> dict:
+		for f in self.fields.keys():
+			field = self.fields[f]			
+			document[field.target] = field.analyze(document[f])
+
+	def add_analyzer(self,analyzer):
+		self.analyzers[analyzer["name"]] = Analyzer(analyzer["pipeline"],self.nlp)
+
+	def add_field(self,field):
+		analyzer = self.analyzers[field["analyzer"]]
+		self.fields[field["source"]] = Field(field,analyzer)
 
 	def __init__(self,config):
-		self.fields = {}
-		for field in config:
-			self.add(field)
 
-"""
-[
-	{
-		"field":"title",
-		"pipeline":[
-			"html_strip",
-			"patternize",
-			"tokenize",
-			"entitize",
-			"lemmatize",
-			"payload"
-		]
-	},
-	{
-		"field":"content",
-		"pipeline":[
-			"html_strip",
-			"patternize",
-			"tokenize",
-			"entitize",
-			"lemmatize",
-			"payload"
-		]
-	}
-]
-"""
+		if "model" not in config.keys():
+			raise ValueError("Yo! I need a model for spacy to load! Specify it as a model property in your config!")
+		
+		self.config = config
+
+		self.nlp = spacy.load(self.config["model"])
+
+		self.analyzers = {}
+		for analyzer in config["analyzers"]:
+			self.add(analyzer)
+
+		self.fields = {}
+		for field in config["fields"]:
+			self.add(field)
