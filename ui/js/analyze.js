b2634fdade47471396cd0ef4033c08081213e44c
Max Irwin <max_irwin@yahoo.com>
2020-09-28 11:49:01 -0400

    Analyzer debug

--- ui/js/analyze.js
+++ ui/js/analyze.js
@@ -1,11 +1,57 @@
+//----------------------------------------------------------
+//Gets the querystring value for the specified key
+$.querystring = (function(key,url){
+    url = url || window.location.search;
+    key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]"); 
+    var regex = new RegExp("[\\?&]"+key+"=([^&#]*)"); 
+    var results = regex.exec( url );
+    return (!results)?"":decodeURIComponent(results[1].replace(/\+/g, " "));
+}).bind($);
+
+//----------------------------------------------------------
+
+var analyze = function(text,analyzer) {
+    uri = '/analyze/' + analyzer + '?debug=true&text=' + encodeURI(text);
+    var ajaxTime= (new Date()) - 0;
+    var data = $("#data");
+    var debug = $("#debug");        
+    $.get(uri,function(res,status) {
+        var totalTime = (new Date())-ajaxTime;
+        if (status=="success") {
+            data.text(res.data)
+            for(var i=0;i<res.debug.length;i++) {
+                $(`<tr>
+                    <td>${res.debug[i].name}</td>
+                    <td>${res.debug[i].time}</td>
+                    <td>${res.debug[i].debug}</td>
+                    </tr>`).appendTo(debug)
+            }
+            $(`<tr>
+                <td>Round Trip:</td>
+                <td><strong>${totalTime}</strong></td>
+                <td><em>NOTE!  Round trip time includes debug overhead.  "(end)" time measures pure analysis time.</em></td>
+                </tr>`).appendTo(debug)
+
+        }
+    });
+}
+
+//----------------------------------------------------------
+
 $(document).ready(function(){
 
-    var form = $("#form");
-    var submit = $("#submit");
-    var select = $("#analyzers");
-    var text = $("#text");
-    var data = $("#data");
-    var debug = $("#debug");
+    var select = $("#analyzer");
+    var textbox = $("#text");
+
+    text = $.querystring("text")
+    analyzer = $.querystring("analyzer")
+
+    if(text && analyzer) {
+        analyze(text,analyzer)
+        textbox.val(text)
+    }
+
+    // ------------------------------------------------
 
     $.get('/analyzers',function(res,status) {
         console.log(status)
@@ -14,43 +60,10 @@ $(document).ready(function(){
                 name = res.data[i]
                 option = $("<option value=\""+name+"\">"+name+"</option>");
                 select.append(option)
-            }
+            }            
+            if (analyzer) select.val(analyzer);
         }
     });
-
-    // ------------------------------------------------
-
-    var analyze = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        analyzer = select.val();
-        text = text.val();
-        uri = '/analyze/' + analyzer + '?debug=true&text=' + encodeURI(text);
-        var ajaxTime= (new Date()) - 0;
-        $.get(uri,function(res,status) {
-            var totalTime = (new Date())-ajaxTime;
-            if (status=="success") {
-                data.text(res.data)
-                for(var i=0;i<res.debug.length;i++) {
-                    $(`<tr>
-                        <td>${res.debug[i].name}</td>
-                        <td>${res.debug[i].time}</td>
-                        <td>${res.debug[i].debug}</td>
-                        </tr>`).appendTo(debug)
-                }
-                $(`<tr>
-                    <td>Round Trip:</td>
-                    <td><strong>${totalTime}</strong></td>
-                    <td><em>NOTE!  Round trip time includes debug overhead.  "(end)" time measures pure analysis time.</em></td>
-                    </tr>`).appendTo(debug)
-
-            }
-        });
-        return false;
-    }
-
-    //form.on('submit',analyze)
-    submit.on('click',analyze)
 
     // ------------------------------------------------
 
