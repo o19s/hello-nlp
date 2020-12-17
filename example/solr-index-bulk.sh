db40016d5e2be689d3c84710749630a586265e41
Max Irwin <max_irwin@yahoo.com>
2020-12-17 10:24:28 -0500

    Example fix

--- example/solr-index-bulk.sh
+++ example/solr-index-bulk.sh
@@ -2,5 +2,5 @@
 echo "Testing bulk enrichment and indexing..."
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data @blog-posts-one.json \
+  --data @blog-posts.json \
   http://localhost:5050/bulk/osc-blog
