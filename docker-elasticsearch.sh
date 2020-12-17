800bf4acf221d68cbe5b038b0f343cf80d58bccd
Max Irwin <max_irwin@yahoo.com>
2020-12-17 17:02:23 -0500

    Easy docker scripts

--- docker-elasticsearch.sh
+++ docker-elasticsearch.sh
@@ -1,3 +1,6 @@
+# Everything will be running when you see something like:
+# "hello-nlp-elasticsearch_1  | INFO:uvicorn.error:Uvicorn running on http://0.0.0.0:5055 (Press CTRL+C to quit)"
+
 docker-compose -f docker-compose-elasticsearch.yml down
 docker-compose -f docker-compose-elasticsearch.yml build
 docker-compose -f docker-compose-elasticsearch.yml up
